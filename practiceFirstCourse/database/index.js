de8d1bcd411113510641804ba0c025f631c6aa7d
Se1emine <youandibutnotwe@gmail.com>
2024-07-01 23:09:19 +0300

    Revert "++"
    This reverts commit 1e2e24a0bd8b1ad0d4c29a573eacc611adc2283b.

--- practiceFirstCourse/database/index.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const database = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.sqlite",
-  logging: false,
-});
-
-module.exports = { database };
