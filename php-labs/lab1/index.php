da3712dc7735bac8f2e981ce6a3369c76f10cb11
Danil <youandibutnotwe@gmail.com>
2025-02-20 21:27:08 +0200

    add php-labs

---
+++ php-labs/lab1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "<br>";
+echo "Hello, World with echo!";
+
+print "<br>";
+print "Hello, World with print!";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Количество дней: " . $days . "<br>";
+echo "Сообщение: " . $message . "<br>";
+
+echo "Количество дней: $days <br>";
+echo "Сообщение: $message <br>";
+
+?>
+
